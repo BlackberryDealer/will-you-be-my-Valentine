7ffadee344b5e082525c4343b8f85e6736d6b8fc
BlackberryDealer <dylanhsx@gmail.com>
2025-02-13 22:49:30 +0800

    s

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.getElementById("no-btn").addEventListener("mouseover", function() {
-    const x = Math.random() * window.innerWidth * 0.6;
-    const y = Math.random() * window.innerHeight * 0.6;
-    this.style.transform = `translate(${x}px, ${y}px)`;
+    const btn = this;
+    const maxX = window.innerWidth - btn.offsetWidth;
+    const maxY = window.innerHeight - btn.offsetHeight;
+    
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+    
+    btn.style.left = `${randomX}px`;
+    btn.style.top = `${randomY}px`;
 });
